4cacf0dc8ff04664197defa69742255eaf74b00c
Kun Huang <gareth@openstack.org>
2014-03-06 01:39:36 +0800

    add my git config script

---
+++ git_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author: Kun Huang <academicgareth@gmail.com>
+
+git config --global user.name "Kun Huang"
+git config --global user.email "gareth@openstack.org"
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.ui auto
+git config --global diff.tool meld
+git config --global merge.tool meld
+git config --global core.editor vim
