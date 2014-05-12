204644390a81f3510b12516d8dfd9c44014f403a
Kun Huang <gareth@openstacker.org>
2014-05-12 20:07:51 +0800

    update git_script.sh

--- git_script.sh
+++ git_script.sh
@@ -2,11 +2,12 @@
 # Author: Kun Huang <academicgareth@gmail.com>
 
 git config --global user.name "Kun Huang"
-git config --global user.email "gareth@openstack.org"
+git config --global user.email "gareth@openstacker.org"
 git config --global color.diff auto
 git config --global color.status auto
 git config --global color.branch auto
 git config --global color.ui auto
-git config --global diff.tool meld
-git config --global merge.tool meld
+git config --global diff.tool bc3
+git config --global difftool.prompt false
+git config --global merge.tool bc3
 git config --global core.editor vim
