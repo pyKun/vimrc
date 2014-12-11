c1ac07ac277ed3a56ede810a1eb716ab8363ba4f
Kun Huang <gareth@openstacker.org>
2014-12-11 11:44:11 +0800

    remove difftool

--- git_script.sh
+++ git_script.sh
@@ -7,7 +7,5 @@ git config --global color.diff auto
 git config --global color.status auto
 git config --global color.branch auto
 git config --global color.ui auto
-git config --global diff.tool bc3
 git config --global difftool.prompt false
-git config --global merge.tool bc3
 git config --global core.editor vim
